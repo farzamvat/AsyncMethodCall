742655aba323af0665ab47cc9d422dbbe6c33dde
farzamvat <farzam.vat@gmail.com>
2016-06-02 17:31:59 +0430

    Add files via upload

---
+++ AsyncCall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async {
+    public class AsyncCall<T,U> {
+        public U firstFunctionResult;
+        private delegate U AsyncCaller<T, U>(T arg);
+        public AsyncCall() {
+        }
+        public void Call(Func<T,U> func, AsyncCallback callback, T firstFuncParam, object secondFuncParam) {
+            AsyncCaller<T,U> caller = new AsyncCaller<T,U>(func);
+
+            IAsyncResult result = caller.BeginInvoke(firstFuncParam, 
+                new AsyncCallback(callback),secondFuncParam);
+
+            result.AsyncWaitHandle.WaitOne();
+
+            firstFunctionResult = caller.EndInvoke(result);
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async {
+    class Program {
+        static void Main(string[] args) {
+
+            object secondFuncParam;
+            AsyncCall<int, string> promise = new AsyncCall<int, string>();
+            promise.Call(test,
+                delegate(IAsyncResult ar) {
+                    Console.WriteLine( ar.AsyncState + " this is callback function.");
+                }, 12345, secondFuncParam : "Hello");
+
+            Thread.Sleep(1000);
+            Console.WriteLine(promise.firstFunctionResult);
+
+        }
+        public static string test(int x) {
+            return "First function to call asynchronously " + x.ToString();
+        }
+
+    }
+}
